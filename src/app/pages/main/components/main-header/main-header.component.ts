99c8058bf9c3edcd35e6a540e17672cab5e69997
asphodel6 <harmsss@mail.ru>
2023-11-25 02:05:49 +0500

    edit header

--- src/app/pages/main/components/main-header/main-header.component.ts
+++ src/app/pages/main/components/main-header/main-header.component.ts
@@ -1,14 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgClass} from '@angular/common';
 
 @Component({
   selector: 'main-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgClass],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainHeaderComponent {
+  isFocused: boolean = false;
+  imgPath: string = 'assets/images/lupa.svg';
 
+  onFocus(): void {
+    this.isFocused = true;
+    this.imgPath = 'assets/images/lupa-active.svg';
+  }
+
+  onBlur(): void {
+    this.isFocused = false;
+    this.imgPath = 'assets/images/lupa.svg';
+  }
 }
